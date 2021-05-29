81d9e76d027cab1fa56f55e3cab0408fc50517e8
Carlangas11 <73184352+Carlangas11@users.noreply.github.com>
2021-05-28 23:16:58 -0400

    commit heroku

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const index = require('./services/index')
+const get = require('./services/get');
 const {PORT} = process.env
 
 const app = express()
@@ -17,7 +17,7 @@ app.use(cors())
 app.use(express.json())
 app.use(cookieParser());
 app.use(morgan('dev'));
-app.use(express.static('build')) //usado para correr el front.
+// app.use(express.static('build')) //usado para correr el front.
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true}))
 
@@ -25,9 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true}))
 
 
   //rutas
-app.get('/', (req,res)=>{
-    res.send('ok')
-})
+app.get('/', get)
 
 
 
