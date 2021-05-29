c373d2a2c6262615ee7d3919f71cbc05b9d723a5
Carlangas11 <73184352+Carlangas11@users.noreply.github.com>
2021-05-28 23:38:03 -0400

    heroku front

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cors())
 app.use(express.json())
 app.use(cookieParser());
 app.use(morgan('dev'));
-// app.use(express.static('build')) //usado para correr el front.
+app.use(express.static('build')) //usado para correr el front.
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true}))
 
